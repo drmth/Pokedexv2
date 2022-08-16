8d282865a36e621a6e36b1a210aea0e5dd55338b
drmth <79118141+drmth@users.noreply.github.com>
2022-08-16 13:08:01 +0200

    data imported and displayed on home page

---
+++ Pokedexv2/src/app/components/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Pokemon } from 'src/app/interfaces/pokemon';
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrls: ['./pokemon-list.component.scss']
+})
+export class PokemonListComponent implements OnInit {
+  @Input() pokemon : Pokemon | undefined;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
