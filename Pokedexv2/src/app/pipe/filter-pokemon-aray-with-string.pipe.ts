1856ffb0e75fa95a9214c8a05eb0dc3a07f310f7
drmth <79118141+drmth@users.noreply.github.com>
2022-08-25 00:20:23 +0200

    user research filter pokemon list

---
+++ Pokedexv2/src/app/pipe/filter-pokemon-aray-with-string.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { filter } from 'rxjs';
+import { PokemonItemComponent } from '../components/pokemon-item/pokemon-item.component';
+import { Pokemon } from '../interfaces/pokemon';
+
+@Pipe({
+  name: 'filterPokemonArayWithString',
+})
+export class FilterPokemonArayWithStringPipe implements PipeTransform {
+  transform(pokemonsList: Pokemon[], userResearch: string): Pokemon[] {
+    if (userResearch === '') return pokemonsList;
+    let filteredListOfPokemon = [] as Pokemon[];
+    pokemonsList.forEach((pokemon: Pokemon) => {
+      if (pokemon.name.includes(userResearch)) {
+        filteredListOfPokemon.push(pokemon);
+      }
+    });
+    console.log(filteredListOfPokemon);
+    return filteredListOfPokemon;
+  }
+}
