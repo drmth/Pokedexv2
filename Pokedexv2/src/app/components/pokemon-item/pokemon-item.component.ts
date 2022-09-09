9413fd056c5ccf6a18f78d43f7c0b8e2b2433cb6
drmth <79118141+drmth@users.noreply.github.com>
2022-09-09 16:20:54 +0200

    add stat chart on pokemon item

--- Pokedexv2/src/app/components/pokemon-item/pokemon-item.component.ts
+++ Pokedexv2/src/app/components/pokemon-item/pokemon-item.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Pokemon } from 'src/app/interfaces/pokemon';
 import { Chain, EvolutionChain } from 'src/app/interfaces/evolution-chain';
 import { PokemonService } from 'src/app/services/pokemon.service';
 import { FlavorTextEntry } from 'src/app/interfaces/pokemon-species';
+import Chart from 'chart.js/auto/auto.mjs';
 
 @Component({
   selector: 'app-pokemon-item',
   templateUrl: './pokemon-item.component.html',
   styleUrls: ['./pokemon-item.component.scss'],
 })
-export class PokemonItemComponent implements OnInit {
+export class PokemonItemComponent implements OnInit, AfterViewInit {
+  @ViewChild('barCanvas') barCanvas: ElementRef | undefined;
+  barChart: Chart | undefined;
+
   currentPokemonName: string | undefined;
   currentPokemon: Pokemon | undefined;
   evolution: string[] = [];
@@ -27,6 +37,67 @@ export class PokemonItemComponent implements OnInit {
       this.currentPokemon = this.getDetailedPokemon(this.currentPokemonName);
       this.getPokemonEvolutionChain(this.currentPokemon.id);
     });
+  }
+
+  ngAfterViewInit(): void {
+    this.barChartStat();
+  }
+
+  barChartStat() {
+    this.barChart = new Chart(this.barCanvas?.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: this.getChartLabel(),
+        datasets: [
+          {
+            label: '# of Votes',
+            data: this.getChartData(),
+            backgroundColor: this.getChartColor(),
+            borderColor: this.getChartColor(),
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }
+
+  getChartLabel(): string[] {
+    let labelChart: string[] = [];
+    this.currentPokemon?.stats.forEach((stat) => {
+      labelChart.push(stat.stat.name);
+    });
+    return labelChart;
+  }
+
+  getChartData(): number[] {
+    let dataChart: number[] = [];
+    this.currentPokemon?.stats.forEach((stat) => {
+      dataChart.push(stat.base_stat);
+    });
+    return dataChart;
+  }
+
+  getChartColor(): string[] {
+    let chartColor: string[] = [];
+    if (this.currentPokemon?.stats) {
+      for (var i = 0; i < this.currentPokemon?.stats.length; i++) {
+        chartColor.push(
+          `rgba(
+            ${Math.floor(Math.random() * 255)},
+            ${Math.floor(Math.random() * 255)},
+            ${Math.floor(Math.random() * 255)},
+            0.2)`
+        );
+      }
+    }
+    return chartColor;
   }
 
   getDetailedPokemon(pokemonName: string): Pokemon {
